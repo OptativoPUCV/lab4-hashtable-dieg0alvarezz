d091b65268f1a4181575f88de61f61d37990157f
dieg0alvarezz <163463258+dieg0alvarezz@users.noreply.github.com>
2024-04-25 23:37:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -78,12 +78,12 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
-  HashMap * map = (HashMap *)malloc(sizeof(HashMap));
-  //map->buckets = (Pair *)calloc(capacity,sizeof(Pair));
-  map->size = 0;
-  map->capacity = capacity;
-  map->current = -1;
-  return map;
+    HashMap * map = (HashMap *)malloc(sizeof(HashMap));
+    map->buckets = (Pair *)calloc(capacity,sizeof(Pair));
+    map->size = 0;
+    map->capacity = capacity;
+    map->current = -1;
+    return map;
 
 }
 void eraseMap(HashMap * map,  char * key) {    
